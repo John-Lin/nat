ed19fcae75f3f05bdc11fcca36d82cfa3ae5adce
John-Lin <linton.tw@gmail.com>
2016-05-24 21:59:41 +0800

    send_packet method

--- helper/ofp_helper.py
+++ helper/ofp_helper.py
@@ -12,3 +12,18 @@ def add_flow(datapath, priority, match, actions, buffer_id=None):
         mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                 match=match, instructions=inst)
     datapath.send_msg(mod)
+
+
+def send_packet(datapath, pkt, port):
+    ofproto = datapath.ofproto
+    parser = datapath.ofproto_parser
+    pkt.serialize()
+
+    data = pkt.data
+    actions = [parser.OFPActionOutput(port=port)]
+    out = parser.OFPPacketOut(datapath=datapath,
+                              buffer_id=ofproto.OFP_NO_BUFFER,
+                              in_port=ofproto.OFPP_CONTROLLER,
+                              actions=actions,
+                              data=data)
+    datapath.send_msg(out)
